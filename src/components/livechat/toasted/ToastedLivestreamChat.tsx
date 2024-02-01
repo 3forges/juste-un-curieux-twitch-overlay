ada71025fd19f4dcf6bf31144ad5fd58c56fe808
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-02-02 00:29:08 +0100

    added button to switch channel name

--- src/components/livechat/toasted/ToastedLivestreamChat.tsx
+++ src/components/livechat/toasted/ToastedLivestreamChat.tsx
@@ -140,6 +140,27 @@ export default /* async */ function ToastedLivestreamChat({ channel, channelOwne
     return (
         <>
             <div className={`z-7 bg-[rgba(138,228,240,0.4)] absolute grid justify-items-center items-center min-w-[80%] min-h-[50%]`} >
+
+                {// <!--Input + button to chanch channel -->
+                }
+                <div class="fixed top-5 flex w-full max-w-[24rem]">
+                    <div class="relative h-12 w-full min-w-[200px]">
+                        <input type="email"
+                            class="peer h-full w-full rounded-[7px] border border-purple-400 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-purple-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-purple-400 placeholder-shown:border-t-purple-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:border-purple-200"
+                            placeholder=" " value="" />
+                        <label
+                            class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-purple-400 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-purple-400 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-purple-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-purple-500">
+                            Twitch Channel
+                        </label>
+                    </div>
+                    <button 
+                        class="!absolute right-1 top-1 bg-purple-600 disabled:bg-purple-200 select-none rounded py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-purple-200 focus:border-purple-500 border-1 focus:border-2"
+                        type="button">
+                        connect
+                    </button>
+                </div>
+
+
                 <ul role="list" class="divide-y divide-gray-100">
                     {//<Toaster expand duration={5} pauseWhenPageIsHidden toastOptions={{}} position="bottom-center" />
 
